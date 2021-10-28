dac6aba122ad3cdb2317caf609f1b2f0dbf2be22
MateoFernandezBanchi <matefbanchi@gmail.com>
2021-10-28 19:26:05 -0300

    contador

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -4,15 +4,15 @@ import './ItemCount.css'
 const ItemCount = () => {
     
     const [count, setCount] = useState(0)
-  
+    let stock = 5
     
     return (
        
         <div>
             <button className='buttonCount'  onClick={()=>{if (count > 0) {setCount(count-1)} else {}; console.log(count)}} > - </button>
             <span className='white'>{count}</span>
-            <button className='buttonCount' onClick={()=>{if (count <=5) {setCount(count+1)} else {}; console.log(count)}} > + </button>
-              
+            <button className='buttonCount' onClick={()=>{if (count <= stock) {setCount(count+1)} else {}; console.log(count)}} > + </button>
+            <button className='buttonCount' > Agregar al carrito </button>
         </div>
     )
 }
