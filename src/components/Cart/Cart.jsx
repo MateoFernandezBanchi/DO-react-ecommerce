a359d88ca5a4ab8cececca450e9eae4ba1050733
MateoFernandezBanchi <matefbanchi@gmail.com>
2021-12-01 12:00:18 -0300

    form modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,53 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { useCartContext } from '../../context/CartContext'
 import {Link} from 'react-router-dom'
 import './Cart.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import {getFirestore} from '../../services/getFirestore';
-import firebase from 'firebase';
-import ModalBuyer from '../ModalBuyer/ModalBuyer'
+
 import FormatPrice from "../../hooks/FormatPrice";
+import FormBuyer from './FormBuyer'
+
 
 export const Cart = () => {
   
     const { CartList, borrarCarrito, borrarItem, cartTotal} = useCartContext ()
-    const [formData, setFormData] = useState ( {
-      name: '',
-      email: '',
-      phone: ''
-
-    } ) 
-    const [error, guardarError] = useState (false)
-    const [orderId, setOrderId] = useState ()
-    const generarOrden = (e) => {
-          e.preventDefault()
-          if (formData.email.trim() === '' || formData.name.trim() === '' || formData.phone.trim()=== '') {
-            guardarError(true)
-            return;
-          } else {
-          guardarError(false)
-          let orden = {}
-          orden.date = firebase.firestore.Timestamp.fromDate(new Date());
-          orden.buyer = formData
-          orden.total = cartTotal
-          orden.items = CartList.map (cartItem => {
-            const id = cartItem.id;
-            const nombre = cartItem.nombre;
-            const precio = cartItem.subtotal;
-            return {id, nombre, precio}
-          } )
-
-          const dbQuery = getFirestore ()
-          dbQuery.collection ('orders').add(orden)
-          .then(({id} ) => { 
-            setOrderId(id)  })
-          .then(resp => console.log(resp))
-          .catch (err=> console.log (err))   
-              console.log (orderId)
-    }}
-
-    const handleChange = (e) => { setFormData ( {...formData,[e.target.name]: e.target.value})}
 
     return (
       <div>
@@ -110,27 +74,7 @@ export const Cart = () => {
                 </table>
               </div>
 
-              <div className="container formContainer col-lg-3">
-                {error? <p className='btn-danger'>todos los campos son obligatorios</p> : null}
-                <form className="d-flex flex-column container pt-5 pb-5 formContainer" 
-                onSubmit={generarOrden} onChange={handleChange}>
-                  <div className='form-group'>
-                    <input className="form-control" type="text" name="name" placeholder="nombre" 
-                    value={formData.name}></input>
-                  </div>
-                  <div className='form-group'>
-                    <input className="form-control" type="text" 
-                    name="phone" placeholder="telefono"value={formData.phone}></input>
-                  </div>
-                  <div className='form-group'>
-                    <input className="form-control" type="text" 
-                    name="email" placeholder="email" value={formData.email}></input>
-                  </div>
-                  <div>
-                    <ModalBuyer data= {formData} total= {cartTotal} errorValue= {error} id= {orderId} /> 
-                  </div>
-                </form>
-              </div>
+              <FormBuyer />
                   
               </div> 
             </>                  
--- src/components/Cart/FormBuyer.jsx
+++ src/components/Cart/FormBuyer.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {getFirestore} from '../../services/getFirestore';
+import firebase from 'firebase';
+import ModalBuyer from './ModalBuyer'
+import { useCartContext } from '../../context/CartContext'
 const FormBuyer = () => {
+    const { CartList, cartTotal} = useCartContext ()
+
+    const [formData, setFormData] = useState ( {
+        name: '',
+        email: '',
+        phone: ''
+  
+      } ) 
+      const [error, guardarError] = useState (false)
+      const [orderId, setOrderId] = useState ()
+
+    const generarOrden = (e) => {
+        e.preventDefault()
+        if (formData.email.trim() === '' || formData.name.trim() === '' || formData.phone.trim()=== '') {
+          guardarError(true)
+          return;
+        } else {
+        guardarError(false)
+        let orden = {}
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
+        orden.buyer = formData
+        orden.total = cartTotal
+        orden.items = CartList.map (cartItem => {
+          const id = cartItem.id;
+          const nombre = cartItem.nombre;
+          const precio = cartItem.subtotal;
+          return {id, nombre, precio}
+        } )
+
+        const dbQuery = getFirestore ()
+        dbQuery.collection ('orders').add(orden)
+        .then(({id} ) => { 
+          setOrderId(id)  })
+        .then(resp => console.log(resp))
+        .catch (err=> console.log (err))   
+            console.log (orderId)
+  }}
+
+    const handleChange = (e) => { setFormData ( {...formData,[e.target.name]: e.target.value})}
+
     return ( 
         <>
-        
+            <div className="container formContainer col-lg-3">
+                {error? <p className='btn-danger'>todos los campos son obligatorios</p> : null}
+                <form className="d-flex flex-column container pt-5 pb-5 formContainer" 
+                onSubmit={generarOrden} onChange={handleChange}>
+                  <div className='form-group'>
+                    <input className="form-control" type="text" name="name" placeholder="nombre" 
+                    value={formData.name}></input>
+                  </div>
+                  <div className='form-group'>
+                    <input className="form-control" type="text" 
+                    name="phone" placeholder="telefono"value={formData.phone}></input>
+                  </div>
+                  <div className='form-group'>
+                    <input className="form-control" type="text" 
+                    name="email" placeholder="email" value={formData.email}></input>
+                  </div>
+                  <div>
+                    <ModalBuyer data= {formData} total= {cartTotal} errorValue= {error} id= {orderId} /> 
+                  </div>
+                </form>
+              </div>
         
         </>
      );
